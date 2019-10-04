8003bc7141f874594d408d2196c24c3041865fbc
AlphaStyle <torbjorn1991@hotmail.com>
2019-10-04 16:44:12 +0200

    Lyssensor og kalibrering

--- lego/test/Test.java
+++ lego/test/Test.java
@@ -4,76 +4,106 @@ import lejos.robotics.MirrorMotor;
 import lejos.robotics.RegulatedMotor;
 import lejos.hardware.lcd.*;
 import lejos.hardware.motor.*;
-import lejos.hardware.sensor.EV3GyroSensor;
-import lejos.hardware.sensor.EV3TouchSensor;
+import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.robotics.SampleProvider;
+import lejos.hardware.Brick;
+import lejos.hardware.BrickFinder;
 import lejos.hardware.port.Port;
 import lejos.hardware.port.SensorPort;
 import lejos.utility.Delay;
 import java.lang.Math;
 
-class Test {
-  public static void main(String[] args) {
+public class Test {
+    public static void main (String[] args)  throws Exception {
         RegulatedMotor MotorA = MirrorMotor.invertMotor(Motor.A);
         RegulatedMotor MotorC = MirrorMotor.invertMotor(Motor.C);
-        MotorA.setSpeed(450);  // sett hastighet (toppfart = 900)
-        MotorC.setSpeed(450);
 
-        MotorA.forward();
-        MotorC.forward();
-        Thread.sleep(2000);
-        MotorA.stop();
-        MotorC.stop();
+        Brick brick = BrickFinder.getDefault();
+        Port fargePort = brick.getPort("S2"); // fargesensor
+        Port fargePortKorrigering = brick.getPort("S4"); // fargesensor
 
-        LCD.clear();
+        EV3ColorSensor fargeSensor = new EV3ColorSensor(fargePort); 
+        SampleProvider fargeLeser = fargeSensor.getMode("RGB"); 
+        float[] fargeSample = new float[fargeLeser.sampleSize()];  
 
-        // Port vinkelPort = SensorPort.S3;
-        // EV3GyroSensor ev3VinekelSensor = new EV3GyroSensor(vinkelPort);
-        // ev3VinekelSensor.reset();
-        // SampleProvider vinkelSensor = ev3VinekelSensor.getAngleMode();
-        
-        // int value = 0;
+        EV3ColorSensor fargeKorrigering = new EV3ColorSensor(fargePortKorrigering); 
+        SampleProvider fargeLeserKorrigering = fargeKorrigering.getMode("RGB");
+        float[] fargeSampleKorrigering = new float[fargeLeserKorrigering.sampleSize()];  
 
-        // System.out.println("Snu 180 grader");
-        // Motor.C.stop();
-        // while (true) {
-        //     float[] sample = new float[vinkelSensor.sampleSize()];
-        //     vinkelSensor.fetchSample(sample, 0);
-        //     value = (int) sample[0];
+        float svart = kalibrering(fargeLeser, fargeSample);
+        float svartKorr = kalibrering(fargeLeserKorrigering, fargeSampleKorrigering);
+        float grå = (svart + svartKorr) / 2;
+        Thread.sleep(3000);
 
-        //     if (Math.abs(value) >= 180) {
-        //         break;
-        //     }
+        float farge = 0;
+        float fargeKorr = 0;
 
-        //     Motor.A.backward();
-        //     Delay.msDelay(500);
-        // }
+        int topSpeed = 700;
+        int midSpeed = 400;
+        int minSpeed = 100;
+        int accTopSpeed = 6000;
+        int accMinSpeed = 1000;
 
+        int retning = 0;
 
-        // Port trykkPortV = SensorPort.S1;
-        // Port trykkPortH = SensorPort.S4;
-        // EV3TouchSensor ev3TrykkSensorH = new EV3TouchSensor(trykkPortH);
-        // EV3TouchSensor ev3TrykkSensorV = new EV3TouchSensor(trykkPortV);
+        MotorA.setSpeed(topSpeed);  // sett hastighet (toppfart = 900)
+        MotorC.setSpeed(topSpeed);
 
-        // SampleProvider tpH = ev3TrykkSensorH.getTouchMode();
-        // SampleProvider tpV = ev3TrykkSensorV.getTouchMode();
+        MotorA.setAcceleration(accTopSpeed);
+        MotorC.setAcceleration(accTopSpeed);
 
-        // int trykkValueH = 0;
-        // int trykkValueV = 0;
+        while (true) {
+            fargeLeser.fetchSample(fargeSample, 0);
+            fargeLeserKorrigering.fetchSample(fargeSampleKorrigering, 0);
 
-        // while(trykkValueH == 0 && trykkValueV == 0){
-			// float[] sampleH = new float[tpH.sampleSize()];
-			// float[] sampleV = new float[tpV.sampleSize()];
-			// tpH.fetchSample(sampleH, 0);
-			// tpV.fetchSample(sampleV, 0);
-			// trykkValueH = (int)sampleH[0];
-			// trykkValueV = (int)sampleV[0];
+            farge = fargeSample[0]*100;
+            fargeKorr = fargeSampleKorrigering[0]*100;
 
-			// Delay.msDelay(500);
+            System.out.println("svart - hvit");
+            if (farge > grå) {
+                if (fargeKorr > grå) {
+                    System.out.println("Hvit - Hvit");
+                    retning = -1;
+                } else if (fargeKorr < grå) {
+                    System.out.println("Hvit - svart");
+                    retning = 1;
+                }
 
-			// System.out.println("Ikke rørt");
-		// } //while
+                MotorA.setAcceleration(accMinSpeed);
+                MotorC.setAcceleration(accMinSpeed);
+            } else {
+                MotorA.setSpeed(topSpeed);
+                MotorC.setSpeed(topSpeed);
 
-		// System.out.println("Nå er den rørt");
+                MotorA.setAcceleration(accTopSpeed);
+                MotorC.setAcceleration(accTopSpeed);
+            }
+
+            if (retning > 0) {
+                MotorC.setSpeed(midSpeed);
+                MotorA.setSpeed(minSpeed);
+            }
+
+            if (retning < 0) {
+                MotorC.setSpeed(minSpeed);
+                MotorA.setSpeed(midSpeed);
+
+            }
+
+            MotorA.forward();
+            MotorC.forward();
+        }
+    }
+
+    private static float kalibrering(SampleProvider fargeLeser, float[] fargeSample) {
+        int svart = 0;
+
+        for (int i = 0; i < 100; ++i) {
+            fargeLeser.fetchSample(fargeSample, 0);
+            svart += fargeSample[0] * 100;
+        }
+
+        svart = (svart / 100);
+        return svart;
     }
 }
