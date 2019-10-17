a7d39cad8fe8f1a73ba5fc4e39aaea3d2bb678ca
Joakim Skogø <ajlngvand@gmail.com>
2019-10-17 12:31:32 +0200

    Dokumentasjon.

--- src/main/java/lego/Motorhastighet.java
+++ src/main/java/lego/Motorhastighet.java
@@ -1,5 +1,11 @@
 package lego;
 
+/**
+ * Hjelpeklasse for å sette motorhastigheter og akkselerasjon.
+ * @author Torbjørn Øverås
+ * @author Gruppe 11, dataingeniør NTNU, første semester.
+ * @version 1.0.0
+ */
 class Motorhastighet {
   public final static int max = 900;
   public final static int mid = 750;
