744b2da9811753daed08ae7b5e3e285892ffabdf
Joakim Skogø <ajlngvand@gmail.com>
2019-10-17 19:55:52 +0200

    Dokumentasjon

--- src/main/java/lego/Motorhastighet.java
+++ src/main/java/lego/Motorhastighet.java
@@ -11,12 +11,26 @@ package lego;
  * @author Gruppe 11, dataingeniør NTNU, første semester.
  * @version 1.0.0
  */
+ 
 class Motorhastighet {
-  public final static int max = 1000;
-  public final static int mid = 600;
-  public final static int min = 250;
-  public final static int maxAcc = 8000;
-  public final static int minAcc = 4000;
+  public final static int max = 1000; // Full fart framover
+  public final static int mid = 600; // Svinghastighet (høy)
+  public final static int min = 250; // Svinghastighet (lav)
+
+  /*
+  * Disse brukes dersom flagget accelrationTest settes i Bil.java.
+  *
+  * maxAcc brukes når roboten går fra svinghastighet og opp.
+  * minAcc brukes når roboten går fra topphastighet ned til svinghastighet.
+  */
+  public final static int maxAcc = 8000; // Akselrasjon lav->høy
+  public final static int minAcc = 4000; // Akselrasjon høy->lav
+
+  /*
+  * Disse brukes kun dersom vi bruker kalkulerte hastigheter. max, mid, min vil da bli ignorert.
+  *
+  * Hver variabel representerer en andel av EV3s rapporterte makshastighet basert på batteri, etc.
+  */
   public final static float maxSpeedFactor = 1.0f;
   public final static float midSpeedFactor = 0.7f;
   public final static float minSpeedFactor = 0.2f;
