5090388b7928b87adafda189c0755ecb58766866
Joakim Skogø <ajlngvand@gmail.com>
2019-10-17 20:14:16 +0200

    Javadoc

--- src/main/java/lego/Retning.java
+++ src/main/java/lego/Retning.java
@@ -2,7 +2,13 @@ package lego;
 
 /**
  * Enum for å bedre lesbarhet.
+ * @author Stian Selvåg
+ * @author Herman Aagaard
+ * @author Henrik Hafsø
+ * @author Joakim Skogø Langvand
+ * @author Erling Sletta
  * @author Torbjørn Øverås
+ * @author Gruppe 11, dataingeniør NTNU, første semester.
  * @version 1.0.0
  */
 
