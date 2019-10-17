4999cd6d2fab433a3b7fb6ad67b330121dc8ffcf
AlphaStyle <torbjorn1991@hotmail.com>
2019-10-17 11:25:38 +0200

    javadoc dokumentasjon

--- src/main/java/lego/Bil.java
+++ src/main/java/lego/Bil.java
@@ -5,10 +5,20 @@ import lejos.robotics.MirrorMotor;
 import lejos.robotics.RegulatedMotor;
 import lejos.hardware.motor.*;
 
+/**
+ * Bil klassen representerer motor egenskapene til en EV3 maskin fra LejOS
+ * Api'et.
+ * @author Torbjørn Øverås
+ * @version 1.0.0
+ */
 public class Bil {
     RegulatedMotor A;
     RegulatedMotor C;
 
+    /**
+     * Reverserer motorene. Det som er fram er nå bak og det som er høyre er nå
+     * venstre
+     */
     public Bil() {
         A = MirrorMotor.invertMotor(Motor.A);
         C = MirrorMotor.invertMotor(Motor.C);
--- src/main/java/lego/Farge.java
+++ src/main/java/lego/Farge.java
@@ -5,6 +5,11 @@ import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.robotics.SampleProvider;
 import lejos.hardware.port.Port;
 
+/**
+ * Fargeklassen representerer en Farge sensor fra LejOS API'et.
+ * @author Torbjørn Øverås
+ * @version 1.0.0
+ */
 public class Farge {
     private EV3ColorSensor sensor;
     private SampleProvider fargeLeser; 
@@ -12,17 +17,32 @@ public class Farge {
     
     private int svart = 0;
 
+    /**
+     * Konstuerer en ny farge sensor fra LejOS API (EV3ColorSensor)
+     * @param port den fysiske porten som blir brukt på EV3 maskinen.
+     * @see Port
+     */
     public Farge(Port port) {
         sensor = new EV3ColorSensor(port); 
         fargeLeser = sensor.getMode("RGB");  
         fargeSample = new float[fargeLeser.sampleSize()];  
     }
 
+    /**
+     * Henter den fargen sensoren ser på i dette øyeblikket.
+     * @return en float som representerer den fargen sensoren ser på i dette
+     * øyeblikket.
+     */
     public float getFarge() {
         this.fargeLeser.fetchSample(this.fargeSample, 0);
         return fargeSample[0]*100;
     }
 
+    /**
+     * Kalibrerer hva fargesensoren ser på i det øyeblikket denne funksjonen
+     * blir utløst.
+     * @return en float som representerer den fargen som fargesensoren ser på.
+     */
     public float kalibrering() {
         for (int i = 0; i < 100; ++i) {
             this.fargeLeser.fetchSample(this.fargeSample, 0);
