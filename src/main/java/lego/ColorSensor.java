5e762609c49a572238ba6b3cc50941ac9a1a28a0
Joakim Skogø <ajlngvand@gmail.com>
2019-11-25 13:07:03 +0100

    Refatoring

---
+++ src/main/java/lego/ColorSensor.java
@@ -0,0 +1,128 @@
+// Copyright (c) 2019 Several authors; see javadoc comment
+//
+// GNU GENERAL PUBLIC LICENSE
+//    Version 3, 29 June 2007
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Hjelpeklasse for fargesensorene i LejOS API.
+ * @author Stian Selvåg
+ * @author Herman Aagaard
+ * @author Henrik Hafsø
+ * @author Joakim Skogø Langvand
+ * @author Erling Sletta
+ * @author Torbjørn Øverås
+ * @author Gruppe 11, dataingeniør NTNU, første semester.
+ * @version 1.1.0
+ */
+
+package lego;
+
+import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.robotics.SampleProvider;
+import lejos.hardware.port.Port;
+import lejos.robotics.Color;
+
+/**
+ * Abstraksjon av fargesensor. Metoder som hasLine() etc forenkler logikken i
+ * andre klasser.
+ */
+public class ColorSensor {
+  private EV3ColorSensor sensor;
+
+  /**
+   * Konstuerer en ny EV3ColorSensor fra LejOS API.
+   *
+   * @param port Fysisk port på EV3-maskinen.
+   */
+  public Farge(Port port) {
+    sensor = new EV3ColorSensor(port);
+  }
+
+  /**
+   * Testmetode. Printer fargeverdi. TODO: Kanskje fjerne denne. Da kan vi ta bort
+   * mange imports. Må da også fjerne call fra App.java.
+   */
+  public void printFargeID() {
+    SampleProvider colorSample = this.sensor.getColorIDMode();
+    float[] sample = new float[colorSample.sampleSize()];
+    colorSample.fetchSample(sample, 0);
+    int colorId = (int) sample[0];
+    String colorName = "";
+    switch (colorId) {
+    case Color.NONE:
+      colorName = "NONE";
+      break;
+    case Color.BLACK:
+      colorName = "BLACK";
+      break;
+    case Color.BLUE:
+      colorName = "BLUE";
+      break;
+    case Color.GREEN:
+      colorName = "GREEN";
+      break;
+    case Color.YELLOW:
+      colorName = "YELLOW";
+      break;
+    case Color.RED:
+      colorName = "RED";
+      break;
+    case Color.WHITE:
+      colorName = "WHITE";
+      break;
+    case Color.BROWN:
+      colorName = "BROWN";
+      break;
+    }
+    System.out.println(colorId + " - " + colorName);
+  }
+
+  /**
+   * Metode for å se om sensoren ser svart.
+   *
+   * @return True hvis sensoren ser svart, false ellers.
+   */
+  public boolean hasLine() {
+    SampleProvider colorSample = this.sensor.getColorIDMode();
+    float[] sample = new float[colorSample.sampleSize()];
+    colorSample.fetchSample(sample, 0);
+    return (int) sample[0] == Color.BLACK ? true : false;
+  }
+
+  /**
+   * Invers av hasLine(). Kun for å gjøre logikk for styring mer lesbar.
+   *
+   * @return True hvis sensoren ikke ser svart, false ellers.
+   */
+  public boolean lostLine() {
+    return !this.hasLine();
+  }
+
+  /**
+   * Metode for å se om sensoren ser svart. Denne er ubrukt i Awesomebot!
+   *
+   * @return True hvis sensoren ser noe annet enn svart eller hvit, false ellers.
+   */
+  public boolean erUbestemt() {
+    SampleProvider colorSample = this.sensor.getColorIDMode();
+    float[] sample = new float[colorSample.sampleSize()];
+    colorSample.fetchSample(sample, 0);
+    int color = (int) sample[0];
+    boolean notBlack = color == Color.BLACK ? false : true;
+    boolean notWhite = color != Color.WHITE ? true : false;
+    return notWhite && notBlack ? true : false;
+  }
+}
---
+++ src/main/java/lego/SpeedSettings.java
@@ -0,0 +1,93 @@
+// Copyright (c) 2019 Several authors; see javadoc comment
+//
+// GNU GENERAL PUBLIC LICENSE
+//    Version 3, 29 June 2007
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Hjelpeklasse for å sette motorhastigheter og akselerasjon.
+ * @author Stian Selvåg
+ * @author Herman Aagaard
+ * @author Henrik Hafsø
+ * @author Joakim Skogø Langvand
+ * @author Erling Sletta
+ * @author Torbjørn Øverås
+ * @author Gruppe 11, dataingeniør NTNU, første semester.
+ * @version 1.1.0
+ */
+
+package lego;
+
+/**
+ * Denne klassen inneholder flere alternative måter å sette hastigheter på
+ * motorene. Dette fordi vi ønsket å prøve å forandre en del ting underveis.
+ */
+public class SpeedSettings {
+
+  /**
+   * Størst hastighet
+   */
+  public final static int max = 1000; // Full fart framover
+
+  /**
+   * Middels hastighet (hastighet på "ytterhjul" i svinger)
+   */
+  public final static int mid = 600;
+
+  /**
+   * Lav hastighet (hastighet på "innerhjul" i svinger)
+   */
+  public final static int min = 250;
+
+  /*
+   * Konstantene nedenfor brukes dersom flagget accelrationTest settes i Bil.java.
+   *
+   * maxAcc brukes når roboten går fra svinghastighet og opp. minAcc brukes når
+   * roboten går fra topphastighet ned til svinghastighet.
+   */
+
+  /**
+   * Største akelerasjonshastighet
+   */
+  public final static int maxAcc = 8000; // Akselrasjon lav->høy
+
+  /**
+   * Laveste akelerasjon shastighet
+   */
+  public final static int minAcc = 4000; // Akselrasjon høy->lav
+
+  /*
+   * Disse brukes kun dersom vi bruker kalkulerte hastigheter. max, mid, min vil
+   * da bli ignorert.
+   *
+   * Hver variabel representerer en andel av EV3s rapporterte makshastighet basert
+   * på batteri, etc.
+   */
+
+  /**
+   * Størst hastighet (faktor)
+   */
+  public final static float maxSpeedFactor = 1.4f;
+
+  /**
+   * Middels hastighet (faktor)
+   */
+  public final static float midSpeedFactor = 0.95f + 0.3f;
+
+  /**
+   * Lav hastighet (faktor)
+   */
+  public final static float minSpeedFactor = 0.30f + 0.3f;
+}
